30593cb86ee943385ad59fd35045624c94d9e358
Rajesh Dhamsaniya <EZDI\rajesh.d@rajeshd.ezdi.local>
2017-05-22 12:13:18 +0530

    updated

---
+++ module4/src/main/java/ezdi/pr4/module4/App.java
@@ -0,0 +1,13 @@
+package ezdi.pr4.module4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World module 4!" );
+    }
+}
