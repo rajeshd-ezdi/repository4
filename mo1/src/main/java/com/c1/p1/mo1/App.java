1f3f6bd854bb3cad09c578eba756b40401c8d776
rajeshd-ezdi <rajesh.d@ezdi.us>
2017-05-30 11:05:28 +0530

    mo1

---
+++ mo1/src/main/java/com/c1/p1/mo1/App.java
@@ -0,0 +1,13 @@
+package com.c1.p1.mo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
